3c2b9e4acfce14fabda62d3777a22d539ba728ce
Andre Eidi Maeda <a256321@dac.unicamp.br>
2023-01-26 17:01:24 -0300

    implements service for pendencies

---
+++ back-end-dart/salvaguardas_backend/bin/services/pendencies/pendencies_service_interface.dart
@@ -0,0 +1,12 @@
+import '../../models/pendencies/pendencies_model.dart';
+
+abstract class IpendenciesService {
+  //Delete one pendencie if it is already updated
+  Future<PendenciesModel?> delete(int id);
+  //return all pendencies
+  Future<List<PendenciesModel>> findAll();
+  //return one required pendencie
+  Future<PendenciesModel?> findOne(int id);
+  //create/update one pendencie
+  Future<PendenciesModel> save(PendenciesModel user);
+}
